dd9d33665c1189203a9b253ee95a08a493a19dcb
Thijs Lambert <thijs.lamb@gmail.com>
2020-03-11 12:16:32 +0100

    express rest api

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,27 @@
+// Express Server
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+import models from './models';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hi');
+// Application-Level Middleware
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use('/users', routes.users);
+app.use('/messages', routes.messages);
+
+// Start Server
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
---
+++ server/src/models/index.js
@@ -0,0 +1,27 @@
+//Dummy Data
+const users = [
+  {
+    id: 0,
+    name: 'Thijs',
+  },
+  {
+    id: 1,
+    name: 'Kevin',
+  },
+];
+
+const messages = [
+  {
+    id: 0,
+    text: 'Hello world',
+  },
+  {
+    id: 1,
+    text: 'Jumping is not a crime',
+  },
+];
+
+export default {
+  users,
+  messages,
+};
